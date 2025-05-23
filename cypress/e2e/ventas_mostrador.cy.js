740f0334019f4df3c842ade44d0b0f54225ecf01
Naim Murad <cachomurad@gmail.com>
2025-05-23 13:07:41 -0300

    23/05

--- cypress/e2e/ventas_mostrador.cy.js
+++ cypress/e2e/ventas_mostrador.cy.js
@@ -22,16 +22,13 @@ describe('Realizar una Venta de Mostrador', () => {
         // Seleccionar la segunda opción "Ventas"
         cy.wait(1000); // Esperar a que la página cargue completamente antes de interactuar con los elementos
         // Seleccionar un producto
-        cy.contains('.q-item__label', 'Producto Fraccionable de Prueba').click(); // Ajusta el selector según la lista de productos
+        cy.contains('.q-item__label', 'Combo Fraccionable de Prueba').click(); // Ajusta el selector según la lista de productos
         cy.wait(1000);
         // Hacer clic en "Pago"
         cy.contains('button', 'PAGO').click(); // Ajusta el selector según el botón de pago
         cy.wait(1000);  
         // Seleccionar el método de pago
         cy.contains('button', 'Efectivo').click(); // Ajusta el selector según el método de pago
-        cy.wait(1000);
-        // Facturación
-        cy.get('button.text-black span').eq(3).click(); // Ajusta el selector según el botón de "Aceptar" o "Continuar"
         cy.wait(1000);
         // Finalizar la venta
         cy.contains('button', 'FINALIZAR').click(); // Ajusta el selector según el botón de finalizar
